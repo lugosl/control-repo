18cb1fc84db3e4eac77e62523affbe282ee72d7c
Sergio Lugo <sllv1982@gmail.com>
2020-06-15 18:00:00 -0400

    commit 5

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,6 +3,16 @@
 ####################################
 include profile::base
 ####################################
+if $facts['is_virtual'] {
+  # Our NTP module is not supported on virtual machines:
+  warning('Tried to include class ntp on virtual machine; this node might be misclassified.')
+} elsif $facts['os']['name'] == 'Darwin' {
+  warning('This NTP module does not yet work on our Mac laptops.')
+} else {
+  # Normal node, include the class.
+  include ntp
+}
+####################################
 
 case $facts['os']['name'] {
   'Solaris':           { include role::solaris } # Apply the solaris class
--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,5 +1,5 @@
 class profile::base {
-
+  notice("try to run this script with -v and -d to see difference between log levels")
   #the base profile should include component modules that will be on all nodes
 ####################################
 #Crear, bloquear y establecer clave a usuarios no roo
@@ -26,3 +26,4 @@ include ::ntp
 include ::ssh
 include ::firewall
 }
+######################################################
