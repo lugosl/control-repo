437a0945d1148431389852f390ab12118b6865df
Sergio Lugo <sllv1982@gmail.com>
2020-06-16 08:47:05 -0400

    commit 6

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,22 +1,6 @@
-####################################
-#Crear, bloquear y establecer clave a usuarios no roo
-####################################
-include profile::base
-####################################
-if $facts['is_virtual'] {
-  # Our NTP module is not supported on virtual machines:
-  warning('Tried to include class ntp on virtual machine; this node might be misclassified.')
-} elsif $facts['os']['name'] == 'Darwin' {
-  warning('This NTP module does not yet work on our Mac laptops.')
-} else {
-  # Normal node, include the class.
-  include ntp
-}
-####################################
-
-case $facts['os']['name'] {
-  'Solaris':           { include role::solaris } # Apply the solaris class
-  'RedHat', 'CentOS':  { include role::redhat  } # Apply the redhat class
-  /^(Debian|Ubuntu)$/: { include role::debian  } # Apply the debian class
-  default:             { include role::generic } # Apply the generic class
-}
+$apache = $::osfamily ? {
+  'Debian' => 'apache2',
+  'RedHat' => 'httpd'
+} 
+$packages = ['memcached'] << $apache
+package {$packages: ensure => installed}
